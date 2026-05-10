f831877b4f7365cca39b1c2eb1f78dda88701a4c
normandboris <manziboris@gmail.com>
2026-05-10 16:14:40 -0400

    connected frontend to live render api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://feedforward-backend-2snz.onrender.com/api';
 
 async function request(path, options = {}) {
   const response = await fetch(API_URL + path, {
